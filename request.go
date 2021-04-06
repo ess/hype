6ed80e926d56d1b65b13963c7cac950191c67599
Dennis Walters <pooster@gmail.com>
2021-04-06 05:17:03 +0000

    Ensured an actual response in every response

--- request.go
+++ request.go
@@ -12,43 +12,33 @@ type Request struct {
 	err    error
 }
 
-func (request *Request) okay() bool {
-	return request.err == nil
-}
-
 func (request *Request) WithHeader(header *Header) *Request {
 	return request.WithHeaderSet(header)
 }
 
 func (request *Request) WithHeaderSet(headers ...*Header) *Request {
-	if request.okay() {
-		for _, header := range headers {
-			request.actual.Header.Add(header.Name, header.Value)
-		}
+	for _, header := range headers {
+		request.actual.Header.Add(header.Name, header.Value)
 	}
 
 	return request
 }
 
 func (request *Request) Response() Response {
-	if !request.okay() {
-		return Response{nil, nil, request.err}
-	}
-
 	response, err := request.raw.Do(request.actual)
 	if err != nil {
-		return Response{nil, nil, err}
+		return Response{response, nil, err}
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return Response{nil, nil, err}
+		return Response{response, nil, err}
 	}
 
 	defer response.Body.Close()
 
 	if response.StatusCode < 200 || response.StatusCode > 299 {
-		return Response{nil, nil, fmt.Errorf("response status: %d", response.StatusCode)}
+		return Response{response, nil, fmt.Errorf("response status: %d", response.StatusCode)}
 	}
 
 	return Response{response, body, nil}
